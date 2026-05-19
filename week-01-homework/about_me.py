bc9736d58883545abc03fb64acda99bb8af187ef
Danielle <danim53@outlook.com>
2026-05-18 18:43:46 -0700

    Add 3 facts about me print to screen

---
+++ week-01-homework/about_me.py
@@ -0,0 +1,13 @@
+"""about_me.py is a script that prints 3 facts about myself"""
+def main() -> None:
+    #declare variables
+    my_name = "Danielle"
+    favorite_food = "Asada Burrito"
+
+    #print info
+    print(f"My name is {my_name}")
+    print(f"My favorite food is an {favorite_food}")
+    print("My goal for this course is to advance my python problem-solving skills")
+
+if __name__ == "__main__":
+    main()
